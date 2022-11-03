2b8d515871fd98b8c83ba6f94fb695e1cf15c668
rithikN <51205051+rithikN@users.noreply.github.com>
2022-11-03 09:38:32 +0000

    Add files via upload

--- package_maker/src/config/py/trm_paths.py
+++ package_maker/src/config/py/trm_paths.py
@@ -3,7 +3,6 @@ from package_maker.src.config.py.trm_nomenclature import *
 
 resolver = {}
 
-
 main_pkg_dir = lucidity.Template(
     'global_pkg_dir',
     '{pkg_dir}/{@global_pkg_name}',
@@ -25,7 +24,6 @@ for_approval_dir = lucidity.Template(
     template_resolver=resolver
 )
 
-
 for_approval_filepath_template = lucidity.Template(
     'for_approval_filepath',
     '{@for_approval_dir}/{@seq_file_name}',
@@ -33,10 +31,18 @@ for_approval_filepath_template = lucidity.Template(
     template_resolver=resolver
 )
 
-resolver_target_list = [GLOBAL_PKG_NAME, LOCAL_PKG_NAME, SHOT_PKG_NAME, SEQ_FILE_NAME,
-                        main_pkg_dir, shot_pkg_dir, for_approval_dir
+vendor_discipline_folder_template = lucidity.Template(
+    'vendor_discipline_folder',
+    '{pkg_dir}/{@vendor_shot_pkg_name}/{discipline}',
+    anchor=lucidity.Template.ANCHOR_END,
+    template_resolver=resolver
+)
+
+resolver_target_list = [
+    GLOBAL_PKG_NAME, LOCAL_PKG_NAME, SHOT_PKG_NAME, SEQ_FILE_NAME,
+    VENDOR_SHOT_PKG_NAME, main_pkg_dir, shot_pkg_dir, for_approval_dir,
+    vendor_discipline_folder_template
 ]
 
 for each_target in resolver_target_list:
     resolver[each_target.name] = each_target
-
